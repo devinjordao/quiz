a7138bd7c7599919ce5e046d2b9e96b723bb43a7
Devin Jordao <devinjordao@gmail.comgit config --global user.name Devin>
2019-11-08 07:22:47 +0000

    Quiz 2

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -6,4 +6,18 @@ class CarsController < ApplicationController
   def new
     @car = Car.new
   end
+
+  def create
+    @car = Car.create(car_params)
+    if @car.invalid?
+      flash[:error] = 'COULD NOT SAVE - the data you entered is invalid.'
+    end
+    redirect_to root_path
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:car_make, :car_model, :car_rating, :caption)
+  end
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,6 @@
 class Car < ApplicationRecord
+  validates :car_make, presence: true, length: { maximum: 30 }
+  validates :car_model, presence: true, length: { maximum: 50 }
+  validates :car_rating, presence: true, :inclusion => 1..10
+  validates :caption, presence: true, length: { maximum: 400 }
 end
